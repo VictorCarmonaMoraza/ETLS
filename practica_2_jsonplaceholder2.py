59a596376814ffca8f02515d103c332a09f7a4e4
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-10 12:27:29 +0100

    Agregar numero de reintentos y numero de reintento

--- practica_2_jsonplaceholder2.py
+++ practica_2_jsonplaceholder2.py
@@ -1,21 +1,26 @@
+import datetime
+
 from prefect import Flow, task
 import requests
 from prefect.utilities.datetimes import retry_delay
 
 
-@task
+@task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def extract():
+    print("Vamos a obtener la respuesta de la API JSONPlaceholder")
     # Realizamos la peticion a la API
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("INFO: Código de estado de la respuesta: {}".format(response.status_code))
     ##Formato JSON
     response = response.json()
     return response
 
 
-@task
+@task(log_stdout=True)
 def load(response):
     output = response[0]['title']
-    print("*****Titulo dle primer onjeto*****")
+    print("INFO: Vm,aos a proceder con la tarea load")
+    print("*****ATENCION****")
     print(str(output))
     print("*****Fin load*****")
 
